3f7607a9a27859937975b87c560ead19eb9e7a04
00038059 <marcelo.gadelha@marinha.mil.br>
2021-12-14 14:49:13 -0300

    api connection

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { getLocalAccessToken } from "../services/service.token"
 
 export const api = axios.create({
-  baseURL: "http://10.5.198.169:3128/",
+  baseURL: "http://127.0.0.1:3128/application-back",
 });
 
 api.interceptors.request.use(async config => {
