e7436bf235591d00c8958d048b0acdd9832086f2
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-01-26 10:16:31 -0300

    CORS by IP

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,10 +2,10 @@ import axios from "axios"
 import { getLocalAccessToken } from "../services/service.token"
 
 export const api = axios.create({
-  // baseURL: "https://10.5.193.20/application-back",
-  baseURL: "http://10.5.198.169:3128/application-back",
+  baseURL: "https://10.5.193.20/application-back",
+  // baseURL: "http://127.0.0.1:3128/application-back",
   headers:{
-    'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Origin': '*',
     // 'Authorization': 'Bearer '+ authHeader(),
   }
 });
